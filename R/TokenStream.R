be5bba172137b737fee1e836b6eb9caec556014c
mike <mikefc@coolbutuseless.com>
2018-03-26 16:57:23 +1000

    reorg of internal data for TokenStream

--- R/TokenStream.R
+++ R/TokenStream.R
@@ -11,59 +11,67 @@ TokenStream <- R6::R6Class(
   "TokenStream",
   public = list(
     tokens        = NULL,
+    values        = NULL,
+    types         = NULL,
     position      = 1L,
+    end_of_stream = FALSE,
     initialize = function(tokens) {
       self$tokens <- tokens
+      self$types  <- names(tokens)
+      self$values <- unname(tokens)
       invisible(self)
     },
 
 
     get_token_value = function(offset = 0) {
-      unname(self$tokens[self$position + offset])
+      self$values[self$position + offset]
     },
 
 
     get_token_type = function(offset = 0) {
-      names(self$tokens[self$position + offset])
+      self$types[self$position + offset]
     },
 
 
     expect_type = function(type) {
-      if (is.null(type) || identical(self$get_token_type(), type)) {
+      if (is.null(type) || identical(self$current_type(), type)) {
         return(invisible(TRUE))
       } else {
-        message <- glue("Expected [{type}] at position {self$position} but found [{self$get_token_type()}]: {dQuote(self$get_token_value())}")
+        message <- glue("Expected [{type}] at position {self$position} but found [{self$current_type()}]: {dQuote(self$current_value())}")
         stop(message, call. = FALSE)
       }
     },
 
 
     expect_value = function(value) {
-      if (is.null(value) || identical(self$get_token_value(), value)) {
+      if (is.null(value) || identical(self$current_value(), value)) {
         return(invisible(TRUE))
       } else {
-        message <- glue("Expected [{value}] at position {self$position} but found [{self$get_token_type()}]: {dQuote(self$get_token_value())}")
+        message <- glue("Expected [{value}] at position {self$position} but found [{self$current_type()}]: {dQuote(self$current_value())}")
         stop(message, call. = FALSE)
       }
     },
 
 
     current_type = function() {
-      self$get_token_type(offset = 0)
+      self$types[self$position]
     },
 
 
     current_value = function() {
-      self$get_token_value(offset = 0)
+      self$values[self$position]
     },
 
 
     consume_token = function(type=NULL, value=NULL) {
       self$expect_value(value)
       self$expect_type(type)
-      token <- self$get_token_value()
+      value <- self$current_value()
       self$position <- self$position + 1
-      token
+      if (self$position > length(self$tokens)) {
+        self$end_of_stream <- TRUE
+      }
+      value
     }
   )
 )
