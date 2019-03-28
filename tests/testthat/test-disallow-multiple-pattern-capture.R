6e1a92782712a3eaa7b61dc120a4c01fcadaba40
Thomas Mailund <mailund@birc.au.dk>
2019-03-28 12:43:21 +0100

    Revert "Disallow multiple captured groups in single pattern"
    This reverts commit 5445d4c8517096255ec152879ae091f60c602789.

--- tests/testthat/test-disallow-multiple-pattern-capture.R
+++
@@ -1,9 +0,0 @@
-context("disallow-multiple-pattern-capture")
-
-test_that("mutiple capture groups per pattern not allowed", {
-
-  patterns <- c(single = 'a', multi = '(b)|(c)')
-  text     <- "abc"
-  expect_error(lex(text, patterns), "Multiple capture")
-
-})
